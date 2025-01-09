32d374ab5d2d689e086602bc1552fc1bca1b3337
22120157 <namkhanhto23@gmail.com>
2025-01-09 11:34:35 +0700

    feat(client): add basic layout in client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Register from "./pages/user/RegisterPage.jsx";
 import Login from "./pages/user/LoginPage.jsx";
-import Home from "./pages/user/HomePage.jsx";
-import UserLayout from "./layouts/UserLayout.jsx";
+// import Home from "./pages/user/HomePage.jsx";
+// import UserLayout from "./layouts/UserLayout.jsx";
 import AuthLayout from "./layouts/AuthLayout";
-import AdminLayout from "./layouts/AdminLayout";
 import SellerLayout from "./layouts/SellerLayout.jsx";
 import MissingPage from "./pages/MissingPage.jsx";
 import RequireAuth from "./hooks/RequireAuth";
@@ -13,38 +12,9 @@ import OTPVerification from "./pages/user/OTPVerificationPage.jsx";
 import ForgetPassword from "./pages/user/ForgetPasswordPage.jsx";
 import ResetPassword from "./pages/user/ResetPasswordPage.jsx";
 import GoogleAuthHandler from './pages/user/GoogleAuthHandlerPage.jsx';
-import Search from "./pages/user/SearchPage.jsx";
-import CategoryWithProducts from "./components/user/CategoryWithProducts.jsx";
-import ProductDetails from "./components/user/ProductDetails.jsx";
-
-// Admin page components
-import AdminDashboard from "./pages/admin/DashboardPage.jsx";
-import RoleManagement from "./pages/admin/RoleManagementPage.jsx";
-import UserManagement from "./pages/admin/UserManagementPage.jsx";
-import UserDetailPage from "./pages/admin/UserDetailPage.jsx";
-import UserWithOrderPage from "./pages/admin/UserWithOrderPage.jsx";
-import SellerManagement from "./pages/admin/SellerManagementPage.jsx";
-import SellerDetailPage from "./pages/admin/SellerDetailPage.jsx";
-import SellerProductPage from "./pages/admin/SellerWithProductPage.jsx";
-import Cart from "./pages/user/CartPage.jsx";
-import PaymentPage from "./pages/user/PaymentPage.jsx";
-import PaymentSuccess from "./components/user/PaymentSuccess.jsx";
-import PaymentFailure from "./components/user/PaymentFailure.jsx";
-import SellerAnalyticsPage from "./pages/admin/SellerAnalyticsPage.jsx";
-import SellerEditPage from "./pages/admin/SellerEditPage.jsx";
-import ProductManagement from "./pages/admin/ProductManagementPage.jsx";
-import ProductDetailPage from "./pages/admin/ProductManagementDetailPage.jsx";
-
 
 // Seller page components
 import SellerDashboard from "./pages/seller/SellerDashboard.jsx";
-import SellerProductManagement from "./pages/seller/SellerProductManagement.jsx";
-import SellerProductDetail from "./pages/seller/SellerProductDetail.jsx";
-import SellerAddProduct from "./pages/seller/SellerAddProduct.jsx";
-import SellerVoucher from "./pages/seller/SellerVoucher.jsx";
-import SellerEditProduct from "./pages/seller/SellerEditProduct.jsx";
-import SellerInfo from "./pages/seller/SellerInfo.jsx";
-import SellerOrder from "./pages/seller/SellerOrder.jsx";
 
 const ROLES = {
     User: 'User',
@@ -57,24 +27,26 @@ const App = () => {
         <Routes>
             <Route element={<PersistLogin />}>
                 {/* USER ROUTE */}
-                <Route path="/" element={<UserLayout />}>
+                {/* <Route path="/" element={<UserLayout />}>
                     <Route index element={<Home />} />
-               
-                </Route>
+                </Route> */}
 
                 {/* USER ROUTE RequireAuth */}
                 <Route element={<RequireAuth allowedRoles={[ROLES.User, ROLES.Admin]} />}>
-                  
+
                 </Route>
 
                 {/* ADMIN ROUTES */}
                 <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
-                   
+
                 </Route>
-                
+
                 {/* SELLER ROUTE */}
                 <Route element={<RequireAuth allowedRoles={[ROLES.Seller]} />}>
-                   
+                    <Route path="/seller" element={<SellerLayout />}>
+                        <Route index element={<Navigate to="dashboard" replace />} />
+                        <Route index path="dashboard" element={<SellerDashboard />} />
+                    </Route>
                 </Route>
             </Route>
 
@@ -86,7 +58,7 @@ const App = () => {
             </Route>
             <Route path="/auth/forget-password" element={<ForgetPassword />} />
             <Route path="/auth/reset-password" element={<ResetPassword />} />
-            <Route path="/auth/register/verify-otp" element={<OTPVerification />}  />
+            <Route path="/auth/register/verify-otp" element={<OTPVerification />} />
             <Route path="/auth/google/callback" element={<GoogleAuthHandler />} />
 
             {/* ERROR PAGES */}
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,8 +3,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import userReducer from "./reducers/user";
-import adminReducer from "./reducers/admin";
-import sellerReducer from "./reducers/seller";
+// import adminReducer from "./reducers/admin";
+// import sellerReducer from "./reducers/seller";
 
 const persistConfig = {
     key: "root",
@@ -17,8 +17,8 @@ const persistedUserReducer = persistReducer(persistConfig, userReducer);
 const store = configureStore({
     reducer: {
         user: persistedUserReducer,
-        admin: adminReducer, // Không cần persist admin state
-        seller: sellerReducer
+        // admin: adminReducer, // Không cần persist admin state
+        // seller: sellerReducer
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
