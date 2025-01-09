efa9dc6a95102392ff7a9a866d84ef6bbf7b4f4e
22120163 <22120163@student.hcmus.edu.vn>
2025-01-09 10:27:25 +0700

    feat: Enable user and product management routes for admin

--- server/routes/admin/index.route.js
+++ server/routes/admin/index.route.js
@@ -4,8 +4,8 @@ const verifyJWT = require('../../middlewares/verifyJWT.middleware');
 const adminRoutes = (app) => {
     // Routes
     app.use('/api/admin/seller', require('./manageSeller.route'));
-    // app.use('/api/admin/user', require('./manageUser.route'));
-    // app.use('/api/admin/products', require('./manageProduct.route'));
+    app.use('/api/admin/user', require('./manageUser.route'));
+    app.use('/api/admin/products', require('./manageProduct.router'));
     // app.use('/api/admin/statistic', require('./salesStatistics.route'));
 }
 
